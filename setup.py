7a51ca886e03d53217b76d6ecd56e92c402458f7
GitMarco27 <marco.sanguineti@numerical.it>
2021-09-13 17:13:29 +0200

    Solving bugs

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='GitMarco',  # How you named your package folder (MyLib)
     packages=['GitMarco'],  # Chose the same as "name"
-    version='v0.0.1',  # Start with a small number and increase it with every change you make
+    version='V0.0.1',  # Start with a small number and increase it with every change you make
     license='MIT',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description='An Engineering, Data Science and Deep Learning python package',  # Give a short description about your library
     author='Marco Sanguineti',  # Type in your name
