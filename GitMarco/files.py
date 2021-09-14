6934e57a05d5961e468e1543ee1945336cd93833
GitMarco27 <marco.sanguineti@numerical.it>
2021-09-14 17:37:26 +0200

    New functions for deleting a file/folder and copying a file

--- GitMarco/files.py
+++ GitMarco/files.py
@@ -1,6 +1,7 @@
 import os
 import zipfile
 import logging
+import shutil
 
 
 def zip_folder(path: str, name: str) -> None:
@@ -11,6 +12,7 @@ def zip_folder(path: str, name: str) -> None:
 
     Zip a folder and its content
     """
+
     def zipdir(new_path, ziph):
         # ziph is zipfile handle
         for root, dirs, files in os.walk(new_path):
@@ -38,3 +40,50 @@ def unzip_folder(path_to_zip_file: str,
     with zipfile.ZipFile(path_to_zip_file, 'r') as zip_ref:
         zip_ref.extractall(directory_to_extract_to)
     logging.info(f'Folder {path_to_zip_file} correctly unzipped to {directory_to_extract_to}')
+
+
+def copy_file(original: str,
+              target: str) -> None:
+    """
+    :param original: original path
+    :param target: destination path
+    :return: None
+
+    Copy a file from original to target
+    """
+    if os.path.exists(original):
+        shutil.copyfile(original, target)
+        logging.info(f'Copied file {original} to {target}')
+    else:
+        logging.error(f"File {original} not found")
+
+
+def delete_file(path: str) -> None:
+    """
+    :param path: path of the file to be deleted
+    :return: None
+
+    Delete a file
+    """
+    if os.path.exists(path):
+        # removing the file using the os.remove() method
+        os.remove(path)
+        logging.info(f'{path} correctly deleted')
+    else:
+        # file not found message
+        logging.error(f"File {path} not found")
+
+
+def delete_folder(path: str) -> None:
+    """
+    :param path:
+    :return: None
+
+    Delete a folder
+    """
+    try:
+        shutil.rmtree(path)
+        logging.info(f'Folder {path} deleted')
+    except OSError as e:
+        logging.error("Error: %s - %s." % (e.filename, e.strerror))
+
