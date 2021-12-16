38e8bc8058218ccb2dc5b82031d66265702733c4
GitMarco27 <72693100+GitMarco27@users.noreply.github.com>
2021-12-16 11:10:02 +0100

    Trying to fix an import bug

--- GitMarco/__init__.py
+++ GitMarco/__init__.py
@@ -1,4 +1,11 @@
-from GitMarco import *
+from GitMarco.energy import *
+from GitMarco.graphics import *
+from GitMarco.notebooks import *
+from GitMarco.statistics import *
+from GitMarco.tf import *
+from GitMarco.torch import *
+from GitMarco.utils import *
+
 import logging
 
 logging.basicConfig(format='GitMarco - %(levelname)s: %(message)s',
