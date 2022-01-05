9166874d56e182f56a4bcd1cc9b2cb9a531f00fb
GitMarco27 <72693100+GitMarco27@users.noreply.github.com>
2022-01-05 16:20:25 +0100

    Tensorflow implementation of parametric PointNet

--- tests/test_tf.py
+++ tests/test_tf.py
@@ -1,7 +1,9 @@
 from GitMarco.tf import utils, metrics, basic
 import numpy as np
 
-from GitMarco.tf.utils import limit_memory
+from GitMarco.tf.losses import chamfer_distance
+from GitMarco.tf.pointnet import Pointnet
+from GitMarco.tf.utils import limit_memory, random_dataset
 
 
 def test_limit_memory():
@@ -25,6 +27,21 @@ def test_basic_dense_model():
     model.summary()
 
 
+def test_chamfer_loss():
+    x = utils.random_dataset(shape=(32, 1024, 3))
+    y = utils.random_dataset(shape=(32, 1024, 3))
+    chamfer_distance(x, y)
 
 
+def test_pointnet():
+    data = random_dataset(shape=(32, 4096, 3))
+    test_data = random_dataset(shape=(32, 4096, 3))
+    field = random_dataset(shape=(32, 4096, 2))
+    field_test = random_dataset(shape=(32, 4096, 2))
 
+    model = Pointnet(n_points=4096,)
+    model = model.create_model()
+    # model.model_2_image()
+    model.summary()
+    model.compile(loss='mse', optimizer='adam')
+    model.evaluate(data, field)
